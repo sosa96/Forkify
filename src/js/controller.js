70cf9945f6c34443c4b9284b4a3b3ca75b63aa2c
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-04-15 17:39:39 +0200

    Implemented Search Results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,15 +1,12 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 // for old browsers support:
 import 'core-js/stable'; // for polyfilling everything else
 import 'regenerator-runtime/runtime'; // for polyfilling async/await
 
-const recipeContainer = document.querySelector('.recipe');
-
 // https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
@@ -19,6 +16,7 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
 
     // Loading recipe
+    // doesnt return anything so it doesnt need to be stored
     await model.loadRecipe(id);
 
     // Rendering recipe
@@ -28,9 +26,26 @@ const controlRecipes = async function () {
   }
 };
 
-controlRecipes();
+const controlSearchResults = async function () {
+  try {
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Load search results
+    // doesnt return anything so it doesnt need to be stored
+    await model.loadSearchResults(query);
+
+    // Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
