a3b06fbc9f1b244eea1c2a5d7df2f29ecfa17a80
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-04-22 21:16:17 +0200

    Implemented local storage for bookmarks

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a3e86ff3-24cf-4051-bf6d-8c34615f55de';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -11,7 +11,28 @@ const timeout = function (s) {
 
 export const getJSON = async function (url) {
   try {
-    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]); // what occurs first wins race
+    const fetchPro = fetch(url);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]); // what occurs first wins race
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err; // reject err so we handle it in model.js catch
+  }
+};
+
+export const sendJSON = async function (url, uploadData) {
+  try {
+    const fetchPro = fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(uploadData),
+    });
+
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]); // what occurs first wins race
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
